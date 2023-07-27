f15a5df29d60da3e00388efa7c4095ea0f0a4856
Sourcery AI <>
2023-07-27 21:24:08 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -41,25 +41,19 @@ def mouse_click(event, x, y, flags, param):
         normalized_center_coordinates = (center_coordinates[0]/width, center_coordinates[1]/height)
         points_list.append(normalized_center_coordinates)
 
-    
-    # to check if right mouse button was clicked
-    if event == cv2.EVENT_RBUTTONDOWN:
-        pass
-
 
 # Create list with path/to/image and file names
-img_path_list = list()
-file_list = list()
+img_path_list = []
+file_list = []
 
 for filename in os.listdir(directory):
-    if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-        file_root = filename.split(sep='.')
-        file_list.append(file_root[0])
-        img_path_list.append(os.path.join(directory, filename))
-        print(os.path.join(directory, filename))
-        
-    else:
+    if not filename.endswith(".jpg") and not filename.endswith(".jpeg"):
         continue
+
+    file_root = filename.split(sep='.')
+    file_list.append(file_root[0])
+    img_path_list.append(os.path.join(directory, filename))
+    print(os.path.join(directory, filename))
 
 # Annotate, Iterate through each image, and write results
 i = len(file_list)
@@ -70,7 +64,7 @@ while i > 0:
     global cache
     if first_read:
         print('first read')
-        points_list = list()
+        points_list = []
 
         # read image 
         orig_img = cv2.imread(img_path_list[-i])
@@ -82,20 +76,18 @@ while i > 0:
         # Resize image 
         #img = cv2.resize(img, (960, 550))
         orig_img = cv2.resize(orig_img, (0,0), fx=0.3, fy=0.3)
-        
+
         cache = orig_img.copy()
-        
+
         # show image and set Mouse Callback
         cv2.imshow('main_window', cache)
-        cv2.setMouseCallback('main_window', mouse_click)
         first_read = False
 
-    
+
     else:
         cv2.imshow('main_window', cache)
         print('not first read')
-        cv2.setMouseCallback('main_window', mouse_click)
-
+    cv2.setMouseCallback('main_window', mouse_click)
     key = cv2.waitKey(0)
 
     ### ASCII Table can be found at: asciitable.com ###
@@ -106,76 +98,38 @@ while i > 0:
         print("p was pressed")
         if points_list != []:
             points_list.pop()
-    
+
+        # Radius of circle
+        radius = 7
+
+        # Line thickness of 2 px
+        thickness = -1
         for point in points_list:
             height, width, channels = cache.shape
             print(point)
-            # Radius of circle
-            radius = 7
-
             # Blue color in BGR
             color = (0, 0, 255)
 
-            # Line thickness of 2 px
-            thickness = -1
             center_coordinates = (int(point[0]*width), int(point[1]*height))
             print(center_coordinates)
-            
+
             cv2.circle(cache, center_coordinates, radius, color, thickness)     
-            
+
             cv2.imshow('main_window', cache)
-            
-              
-
-    # If spacebar is pressed - Skip Joint
-    # if key == 32:
-    #     img_2 = orig_img.copy()
-    #     height, width, channels = img_2.shape
-    #     img_2 = cv2.resize(img_2, (0, 0), fx=0.3, fy=0.3)
-        
-    #     print('space')
-    #     print(points_list)
-    #     if points_list != []:
-    #         print(points_list.pop())
-    #     print(points_list)
-    #     for point in points_list:
-    #         print(point)
-    #         # Radius of circle
-    #         radius = 7
-
-    #         # Blue color in BGR
-    #         color = (0, 255, 0)
-
-    #         # Line thickness of 2 px
-    #         thickness = -1
-    #         center_coordinates = (int(point[0]*width), int(point[1]*height))
-    #         cv2.circle(img_2, center_coordinates, radius, color, thickness)
-    #         cv2.imshow('main_window', img_2)     
-        
 
 
-    # If Enter key is pressed - Next Image
+
     elif key == 13:
-        # Create Key Point Dictionary and Write JSON file
-        key_pts = dict()
-        for idx, point in enumerate(points_list):
-            key_pts[idx] = point
-        
-        with open('results/' + '{}'.format(file_list[-i]) + '.json', 'w', encoding='utf-8') as f:
+        key_pts = dict(enumerate(points_list))
+        with open(f'results/{file_list[-i]}.json', 'w', encoding='utf-8') as f:
             json.dump(key_pts, f, ensure_ascii=False, indent=4)
             f.close()
 
         # Continue on to next image
         i -= 1
         first_read = True
-        #continue
+            #continue
 
-    # If backspace is pressed - Previous Image
-    # elif key == 8:
-    #     print('backspace mf')
-    #     cv2.setMouseCallback('main_window', mouse_click)
-
-    # if escape key is pressed - Exit Program
     elif key == 27:
         break
 
